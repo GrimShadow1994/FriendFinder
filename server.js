fbde5f5d1462908f2ba6267ec51bf64cc31fe7ee
Mathew <maddaugh1@outlook.com>
2019-05-06 09:27:40 -0700

    updated server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, "./app/public")));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log("Friend Finder app is listening on PORT: " + PORT);
 });
