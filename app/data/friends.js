839ced3bbfa1f91486b44fd1b8666c8bab95731e
Mathew <maddaugh1@outlook.com>
2019-05-06 09:29:36 -0700

    updated data

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: "Mathew",
+        photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/791f45b0-ca20-4926-b07d-6da2ec7ddd10/dcz9lem-a51b7770-3101-409f-8c0f-b0112cddcf8a.jpg",
+        scores: [
+            "1",
+            "5",
+            "5",
+            "1",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "5"
+        ]
+    },]
+    
+
+module.exports = friendsArray;
